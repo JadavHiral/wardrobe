e948778e9d3bf09ce04414f6c39ad3835846c20c
jagruti-codes <er.jagrutisagathiya@gmail.com>
2025-11-30 22:56:29 +0530

    clear cart page

---
+++ User/clear_cart.php
@@ -0,0 +1,16 @@
+<?php
+include_once("db_connect.php");
+session_start();
+
+// Username or guest
+$username = isset($_SESSION['username']) ? $_SESSION['username'] : 'guest';
+
+// Delete all items for this user
+$stmt = $conn->prepare("DELETE FROM add_to_cart WHERE username=?");
+$stmt->bind_param("s", $username);
+$stmt->execute();
+
+// Redirect back to cart
+header("Location: cart_show.php");
+exit;
+?>
